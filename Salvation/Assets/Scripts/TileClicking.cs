c9ea91cd4590e3a559412403dffccc98f788576f
Nicholas Mirchandani <nmirchandani@chapman.edu>
2019-05-08 20:53:03 -0700

    Adds basic Pathfinding

---
+++ Salvation/Assets/Scripts/TileClicking.cs
@@ -0,0 +1,53 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileClicking : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Camera levelCamera;
+    public Grid grid;
+    public Tilemap tilemap;
+    public GameObject player;
+    private IAstarAI playerAI;
+
+    public GameObject tracker;
+
+    void Start()
+    {
+        playerAI = player.GetComponent<IAstarAI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Optimization: Only call onMouseOver if mouse is over tilemap
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = levelCamera.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+            Vector3Int position = grid.WorldToCell(worldPoint);
+            Tile tile = (Tile)tilemap.GetTile(position);
+
+            if (tile != null)
+            {
+
+                tracker.SetActive(true);
+                tracker.transform.position = position + new Vector3(0.5f, 0.5f, -5);
+                playerAI.SearchPath();
+            }
+            else
+            {
+                tracker.SetActive(false);
+            }
+        }
+    }
+}
