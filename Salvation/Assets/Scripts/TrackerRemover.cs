8b91515f45fe2673fff39fd8fe625f631d89a93e
Nicholas Mirchandani <nmirchandani@chapman.edu>
2019-05-08 21:03:05 -0700

    Moves scripts into scripts folder

---
+++ Salvation/Assets/Scripts/TrackerRemover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackerRemover : MonoBehaviour
+{
+
+    public GameObject tracker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: Swap this out with OnPathComplete call to increase efficiency
+        if(Time.frameCount % 5 == 0)
+        {
+            if (transform.position == tracker.transform.position)
+            {
+                tracker.SetActive(false);
+            }
+        }
+    }
+}
