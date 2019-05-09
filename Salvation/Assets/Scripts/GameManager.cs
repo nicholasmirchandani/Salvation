1eb0e5cdb4351e4072a6f701c410f62e519aaec5
Sean Robbins <srobbins@chapman.edu>
2019-05-08 23:52:37 -0700

    Framework of the GameManager Class

--- Salvation/Assets/Scripts/GameManager.cs
+++ Salvation/Assets/Scripts/GameManager.cs
@@ -4,15 +4,126 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    Team charTeam;
+    Team comTeam;
+    bool playerControl;
+    bool movePhase;
+    int selectedUnit;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        movePhase = true;
+        charTeam = new Team();
+        comTeam = new Team();
+        playerControl = true;
+        selectedUnit = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Set the next unit in the team to their turn
+    void setTurn(Team team)
+    {
+        int lastInt = team.units.Count -1; //get the index of the last unit in the team
+        //check to see if the last unit in the team has turn
+        //if it does switch player control so the next team can go
+        //As well as set the turn to the first unit
+        if (team.units[lastInt].hasTurn)
+        {
+            team.units[0].hasTurn = true;
+            selectedUnit = 0;
+            playerControl = !playerControl;
+            return;
+        }
+        bool nextTurn = false;
+        //Iterate through the team and set the turn to the next unit in the team
+        foreach (Unit u in team.units)
+        {
+            if(u.hasDied)
+            {
+                continue;
+            }
+            else if (u.hasTurn)
+            {
+                nextTurn = true;
+                u.hasTurn = !u.hasTurn;
+            }
+            else if(nextTurn)
+            {
+                selectedUnit++;
+                u.hasTurn = true;
+                nextTurn = false;
+            }
+        }
+    }
+
+    void Attack()
+    {
+
+    }
+
+    void ComAttack()
+    {
+
+    }
+
+    void MoveUnit()
+    {
+
+    }
+
+    void ComMoveUnit()
+    {
+
+    }
+
+    //Controls the Logic for Player Actions
+    void PlayerTurn()
+    {
+        if(movePhase)
+        {
+            MoveUnit();
+        }
+        else
+        {
+            Attack();
+        }
+
+        setTurn(charTeam);
+    }
+
+    //Controls the actions for the computer turns
+    void ComTurn()
+    {
+        if (movePhase)
+        {
+            ComMoveUnit();
+        }
+        else
+        {
+            ComAttack();
+        }
+
+        setTurn(comTeam);
+    }
+
+    private void OnMouseOver()
+    {
+        //Left Click
+        if(Input.GetMouseButtonDown(0))
+        {
+
+        }
+        //Right Click
+        if (Input.GetMouseButtonDown(1))
+        {
+
+        }
     }
 }
